b344a05cfc0d68ff7356591ffd89912921b1fbc9
congtiendev <congtiendev@gmail.com>
2023-03-26 00:27:10 +0700

    update 26/3/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
+  darkMode: 'class',
   content: [
     "./node_modules/tw-elements/dist/js/**/*.js",
     "./app/views/**/**/**/*.{html,js,php,blade.php}",
